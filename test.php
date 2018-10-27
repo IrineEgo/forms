f01b2bd65d0b5e383c2ddde3fdcdf6c5e1804583
Irine Egorenkova <17315833+IrineEgo@users.noreply.github.com>
2018-10-27 09:15:47 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,44 +1,23 @@
-<?php
-    if (isset($_GET['number']) === false) {
+<?php   
+    $allTests = glob('tests/*.json');
+    if (isset($_GET['number']) === false || $_GET['number'] == '' || $_GET['number'] > count($allTests)-1) {
         header('Location: list.php');
         exit;
     }
+    
     // Получаем файл с номером из GET-запроса
     $allTests = glob('tests/*.json');
     $number = $_GET['number'];
+    
+    /////////////////////   
+    $answers = []; 
+    if(isset($_POST['answers']))
+        $answers = $_POST['answers'];
+   
     $test = file_get_contents($allTests[$number]);
     $test = json_decode($test, true);
-	
-    // Проверка теста и вывод результата
-    if (isset($_POST['check-test'])) {
-        function checkTest($testFile) {
-            foreach ($testFile as $key => $item) {
-                if (!isset($_POST['answer' . $key])) {
-                    echo 'Должны быть решены все задания!';
-                    exit;
-                }
-            }
-        $i = 0;
-        $questions = 0;
-            foreach ($testFile as $key => $item) {
-                $questions++;
-                if ($item['correct_answer'] === $_POST['answer' . $key]) {
-                    $i++;
-                    $infoStyle = 'correct';
-                } else {
-                    $infoStyle = 'incorrect';
-                }
-                // Вывод вопросов и ответов
-                //echo "<div class=\"$infoStyle\">";
-                echo 'Вопрос: ' . $item['question'] . '<br>';
-                echo 'Вы ответили: ' . $item['answers'][$_POST['answer' . $key]] . '<br>';
-                echo 'Правильно: ' . $item['answers'][$item['correct_answer']] . '<br>';
-                echo '</div>';
-                echo '<hr>';
-            }
-            echo '<h4>Количество верных ответов: ' . $i . ' из ' . $questions . '</h4>';
-        }
-    }
+
+    $correct_answers = 0;    
 ?>
 
 <!DOCTYPE html>
@@ -46,30 +25,53 @@
   <head>
     <meta charset="utf-8">
     <title>ТЕСТ</title>
+    <style type="text/css">
+        .green { color:green; }
+        .red { color:red; }
+    </style>
   </head>
-  <body>
-    <a href="<?php echo isset($_POST['check-test']) ? $_SERVER['HTTP_REFERER'] : 'list.php' ?>"><div><< НАЗАД</div></a><br>
-    <?php if (isset($_GET['number']) && !isset($_POST['check-test'])): ?>
-        <form method="POST">
-            <h1><?php echo basename($allTests[$number]); ?></h1>
-            <?php foreach($test as $key => $item):  ?>
+  <body>   
+    <a href="list.php"><< НАЗАД</a><br>
+	
+        <?php if(count($answers) < 3):?>
+            Должны быть решены все задания!            
+        <?php endif?>    
+    
+        <form method="POST">           
+            <h1><?php echo $test['name'] ?></h1>           
+            <?php foreach($test['questions'] as $qkey => $item):  ?>          
             <fieldset>
-                <legend><?php echo $item['question'] ?></legend>
-                <label><input type="radio" name="answer<?php echo $key ?>" value="0"><?php echo $item['answers'][0] ?></label><br>
-                <label><input type="radio" name="answer<?php echo $key ?>" value="1"><?php echo $item['answers'][1] ?></label><br>
-                <label><input type="radio" name="answer<?php echo $key ?>" value="2"><?php echo $item['answers'][2] ?></label><br>
-                <label><input type="radio" name="answer<?php echo $key ?>" value="3"><?php echo $item['answers'][3] ?></label>
+                <legend><?php echo $item['question'] ?></legend>              
+                <?php foreach($item['answers'] as $value => $answer): ?>
+                    <label>
+                        <input type="radio" name="answers[<?=$qkey?>]" value="<?=$value?>" required <?php if(isset($answers[$qkey]) && $value == $answers[$qkey]):?> checked <?php endif;?>>
+                        <?php echo $answer ?>
+                    </label><br>              
+                <?php endforeach; ?>
+
+                <?php if(isset($answers[$qkey])): ?>
+                <?php if($item['correct_answer'] == $answers[$qkey]) $correct_answers++; ?>    
+                
+                <p class="<?php if($item['correct_answer'] == $answers[$qkey]):?>green<?php else:?>red<?php endif?>">
+                    Правильный ответ: <i><?=$item['answers'][$item['correct_answer']]?></i>
+                </p>
+                
+                <?php endif?>  
+				
             </fieldset>
+            
             <?php endforeach; ?>
-			<br>
-			<br>
+            <br>
+            <br>
             <input type="submit" name="check-test" value="Результат">
         </form>
-    <?php endif; ?>
+    
 
     <!-- Проверка результатов теста -->
     <div>
-        <?php if (isset($_POST['check-test'])) echo checkTest($test); ?>
+        <?php if (isset($_POST['check-test'])): ?>
+        <br><br>Всего правильных ответов: <?=$correct_answers?>
+        <?php endif;?>
     </div>
   </body>
 </html>
